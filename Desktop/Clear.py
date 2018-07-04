05c415afbf70b7cc00b0e468a3162ffc2bb69c88
BlasterDinray <jyto7034@gmail.com>
2018-07-05 01:18:19 +0900

    2

--- Desktop/Clear.py
+++ Desktop/Clear.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
         target = ''
         fcount = 0
         scount = 0
+        filenamecount = 0
         try:
                 setfile = open('C://Settings//Settings.txt', 'r')
         except IOError as e:
@@ -32,26 +33,50 @@ if __name__ == '__main__':
                                 del fpaths[fpaths.index(tempd)]
                 for itpath in fpaths:
                         path.append(itpath)
-        try:
-            for dir in path:
-                for file in os.listdir(dir):
-                    if (
-                            file.endswith(".png")  or file.endswith(".mp4")or
-                            file.endswith(".gif")  or file.endswith(".JPG") or
-                            file.endswith(".jpg")):
-                        src = dir + "\\" + file
-                        dsc = target + "\\" + file
-                        print(src, dsc)
-                        shutil.move(src, target)
-                        scount += 1
-        except Exception as e:
-                if e == FileExistsError:
-                        srcfilesize = os.stat(src).st_size
-                        dscfilesize = os.stat(dsc).st_size
-                        if dscfilesize == srcfilesize:
-                                #os.remove(dsc)
-                                scount += 1
+
+                for dir in path:
+                        if os.path.isdir(dir):
+                                for file in os.listdir(dir):
+                                        try:
+                                                if (
+                                                        # file.endswith(".png") or file.endswith(".mp4") or
+                                                        # file.endswith(".gif") or file.endswith(".JPG") or
+                                                        # file.endswith(".jpg")):
+                                                        file.endswith(".asd")):
+                                                        src = dir + "\\" + file
+                                                        dsc = target + "\\" + file
+                                                        print(src, target)
+                                                        shutil.move(src, target)
+                                                        scount += 1
+                                                # else:
+                                                #         print("There is no file!")
+                                        except Exception as e:
+                                                if os.path.exists(dsc):
+                                                        srcfilesize = os.stat(src).st_size
+                                                        dscfilesize = os.stat(dsc).st_size
+                                                        try:
+                                                                if dscfilesize == srcfilesize:
+                                                                        os.remove(dsc)
+                                                                        shutil.move(src, dsc)
+                                                                        scount += 1
+                                                                else:
+                                                                        while True:
+                                                                                filenamecount += 1
+                                                                                chgfile = file[
+                                                                                          :file.__len__() - 4] + "(" + str(
+                                                                                        filenamecount) + ")" + file[
+                                                                                                               file.__len__() - 4:]
+                                                                                checkfile = target + "\\" + chgfile
+                                                                                print(checkfile)
+                                                                                if not os.path.exists(checkfile):
+                                                                                        break
+                                                                        os.rename(file, chgfile)
+                                                                        src = dir + "\\" + chgfile
+                                                                        stat = shutil.move(src, target)
+                                                                        print("pa")
+                                                        except Exception as e:
+                                                                print(e)
+                                                                pass
                         else:
-                                print(target[:4] + "d")
-        finally:
-            print("Success : %d   :   Failed : %d" % (scount, fcount))
+                                print("No exist folder")
+        print("Success : %d   :   Failed : %d" % (scount, fcount))
